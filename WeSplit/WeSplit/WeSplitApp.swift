4f9a68177e38b340f0c60ded79534da59a0a4d3f
Dom Jackson <dom.jackson@bbc.co.uk>
2025-09-19 10:43:31 +0100

    add WeSplit project

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dom Jackson on 08/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
