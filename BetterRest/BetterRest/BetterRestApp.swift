9d1fbef7dbacb436ea3108e6c84b9f804b33e57e
Dom Jackson <dom.jackson@bbc.co.uk>
2025-10-01 08:39:14 +0100

    Day 27 BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dom Jackson on 27/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
